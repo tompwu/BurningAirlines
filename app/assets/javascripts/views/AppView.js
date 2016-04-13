112b654834c74d25ca58707da6d6489eed88f562
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-04-13 19:15:39 +1000

    still battling with the search

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -1,17 +1,19 @@
 var app = app || {};
 
 app.AppView = Backbone.View.extend({
-    el: '#main',
-    events: {
-      'click #searchSubmit' : 'createSearch'
-    },
-    render: function() {
-      var appViewTemplate = $('#appViewTemplate').html();
-      this.$el.html( appViewTemplate );
-      app.flights.each( function(flight) {
-          var flightListView = new app.FlightListView({model: flight});
-          flightListView.render();
+  el: '#main',
+  events: {
+    'click #searchSubmit': 'createSearch'
+  },
+  render: function() {
+    var appViewTemplate = $('#appViewTemplate').html();
+    this.$el.html(appViewTemplate);
+    app.flights.each(function(flight) {
+      var flightListView = new app.FlightListView({
+        model: flight
       });
+      flightListView.render();
+    });
   },
   createSearch: function(event) {
     event.preventDefault();
@@ -19,12 +21,25 @@ app.AppView = Backbone.View.extend({
     destination = destinationfield.value;
     date = datefield.value;
     console.log(origin + " " + destination + " " + date);
-    var SearchResultView = new app.SearchResultView();
-    SearchResultView.render();
+    // var SearchResultView = new app.SearchResultView();
+    // SearchResultView.render();
 
-    app.flights.filter(function (flight) {
-  return flight.get("origin").name === origin && flight.get("destination").name === destination;
-});
+    var search = app.flights.filter(function(flight) {
+      return flight.get("origin").name === origin && flight.get("destination").name === destination;
+    });
+    // for (var i = 0; i < search.length; i++) {
+    //     var searchModel = search[i];
+    //     var flightListView = new app.FlightListView({
+    //       model: searchModel
+    //     });
+    //     flightListView.render();
+    //   }
+
+  }
+  });
+
+
+
 
 // app.flights.filter(function (flight) {
 //   return flight.get("origin").name === "Los Angeles" && flight.get("destination").name === "Sydney";
@@ -36,5 +51,3 @@ app.AppView = Backbone.View.extend({
 // var userDate = "2016-05-13"
 // date.includes( userDate );
 // ===>true
-  }
-});
