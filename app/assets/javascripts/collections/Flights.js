a7abf7e4828a93007e5638637db0983cf9b9326f
David Haynes <dhaynesxx@gmail.com>
2016-04-14 14:59:45 +1000

    fixed so only lists flights once searched

--- app/assets/javascripts/collections/Flights.js
+++ app/assets/javascripts/collections/Flights.js
@@ -5,8 +5,8 @@ app.Flights = Backbone.Collection.extend({
     model: app.Flight,
     initialize: function() {
         this.on('add', function(flight) {
-            var flightListView = new app.FlightListView({model: flight});
-            flightListView.render();
+            // var flightListView = new app.FlightListView({model: flight});
+            // flightListView.render();
         });
     }
 });
