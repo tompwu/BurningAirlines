a05480216f63173a5adbd4eb4154812bfefb4f73
David Haynes <dhaynesxx@gmail.com>
2016-04-15 00:03:16 +1000

    search function fixed so it lists in date_time order

--- app/assets/javascripts/views/AppView.js
+++ app/assets/javascripts/views/AppView.js
@@ -22,6 +22,7 @@ app.AppView = Backbone.View.extend({
 
 // Below tests the search fields upon submit for similar db
 // results and then renders the appropriate results accordingly.
+
     var search = _.filter(app.flights.models, function(flight) {
 
       if (date !== null && origin !== 0 && destination !== 0) {
@@ -49,14 +50,30 @@ app.AppView = Backbone.View.extend({
     // Removes the previous list of results and renders the new
     // according the the above parameters.
 
+    var searchSort = [];
+    var searchDates =[];
+
+    for (var i = 0; i < search.length; i++) {
+        searchDates.push(search[i].attributes.date_time);
+      }
+
+    searchDates = searchDates.sort();
+
+    for (var x = 0; x < searchDates.length; x++) {
+        for (var y = 0; y < search.length; y++) {
+            if (search[y].attributes.date_time === searchDates[x]) {
+              searchSort.push(search[y]);
+            }
+        }
+    }
     this.$el.find('li').remove();
     this.$el.find('ul').remove();
 
     var $headings = $('<ul id="flight-list" class="flight-ul"><li class="displaylist list-heading">Origin</li><li class="displaylist list-heading">Destination</li><li class="displaylist list-heading">Date</li><li class="displaylist list-heading">Seats Available</li></ul>');
     $('.flight-list-style').append($headings);
-    for (var i = 0; i < search.length; i++) {
+    for (var j = 0; j < searchSort.length; j++) {
       var searchView = new app.FlightListView({
-        model: search[i]
+        model: searchSort[j]
       });
       searchView.render();
     }
