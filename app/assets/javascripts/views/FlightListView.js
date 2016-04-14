855d0fe66cb0ecf11ade7bc5abf2a6b67eece882
Tom Ulman <Tom@Toms-MacBook-Pro.local>
2016-04-14 11:19:27 +1000

    merging

--- app/assets/javascripts/views/FlightListView.js
+++ app/assets/javascripts/views/FlightListView.js
@@ -8,13 +8,13 @@ app.FlightListView = Backbone.View.extend({
   render: function() {
     var origin = this.model.attributes.origin.name;
     var destination = this.model.attributes.destination.name;
-
     var dateChange = this.model.get('date_time');
     var date = (dateChange.substr(0,10) + " " + dateChange.substr(11,5));
+    var availableSeats = '';
+
     this.$el.text("Origin: " + origin + ", Destination: " + destination + ", Date: " + date + " Seats Available:");
     this.$el.appendTo('#flight-list');
 
-    var availableSeats = "Seats: ";
   },
   showFlight: function() {
     console.log(this.model.get("id"));
